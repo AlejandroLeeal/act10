4eaedbfc057bb7f84da7f4a5bd51b9792680e318
AlejandroLeeal <alexleeal2508@gmail.com>
2018-10-07 03:02:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/leal/act10/servicio.java
@@ -0,0 +1,24 @@
+package com.example.leal.act10;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.*;
+import java.util.*;
+
+public class servicio extends Service {
+    private final Binder cli = new cliente();
+
+    public class cliente extends Binder {
+        servicio getService() {
+            return servicio.this;
+        }
+    }
+    public servicio() {
+    }
+   
+    @Override
+    public IBinder onBind(Intent intent) {
+        return cli;
+    }
+}
+
